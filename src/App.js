8779625264ccc66ce6bf68c29e5a8e462d90cb70
adilet127 <103016828+adilet127@users.noreply.github.com>
2022-11-05 22:53:20 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import HeaderMenu from './components/HeaderMenu';
+import HeaderInfo from './components/HeaderInfo';
+import Home from './components/Home';
+import Works from './components/Works';
+import Menu from './components/Menu';
+import Order from './components/Order';
+import Cafe from './components/Cafe';
+import Footer from './components/Footer';
+
+
+
+
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderMenu/>
+      <HeaderInfo/> 
+      <Home/> 
+      <Works/>
+      <Menu/>
+      <Order/>
+      <Cafe/>
+      <Footer/>  
+    </div>
+  );
+}
+
+<script src='HeaderMenu.js'/>
+
+export default App;
